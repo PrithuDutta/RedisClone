17d8b1358a7e303d243b89cf7c6bb15acfe15f14
Prithu <prithu@uw.edu>
2026-01-28 21:16:25 -0800

    simple loading and saving commands for the database

---
+++ src/Commands/LoadCommand.java
@@ -0,0 +1,17 @@
+package Commands;
+
+import Database.Database; 
+
+public class LoadCommand implements Commands{
+    @Override
+    public String execute(Database db, String[] args) {
+        if(args.length != 1) {
+            return Protocol.error("ERROR: load command cannot take any arguments");
+        }
+
+        if(!(db.load())) {
+            return Protocol.error("ERROR: DB failed to load"); 
+        }
+        return Protocol.simpleString("DB loaded sucessfully"); 
+    }
+}

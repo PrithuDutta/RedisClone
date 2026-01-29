5d5f2237900d16766d4d00584a1444c8d418745e
Prithu <prithu@uw.edu>
2026-01-28 21:09:13 -0800

    created seilization for the DB for persistent storage

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,8 +1,18 @@
 package Database;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class Database {
+
+
+public class Database implements Serializable{
+    private static final long serialVersionUID = 1L; 
     private final ConcurrentHashMap<String, String> store = new ConcurrentHashMap<>();
 
     public String get(String key) {
@@ -25,4 +35,35 @@ public class Database {
         return store.size();
     }
 
+    public boolean save() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("db.rdb"))) {
+            out.writeObject(store); 
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean load() {
+        File rdb = new File("db.rdb");
+        if (!rdb.exists()) {
+            System.out.println("No save file found. Starting with empty database.");
+            return true;
+        }
+
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(rdb))) {
+            ConcurrentHashMap<String, String> loadedStore = (ConcurrentHashMap<String, String>) in.readObject();
+        
+            store.clear();
+            store.putAll(loadedStore);
+            
+            System.out.println("DB loaded: " + store.size() + " keys.");
+            return true; 
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error loading DB: " + e.getMessage());
+            return false; 
+        }
+    }
 }
